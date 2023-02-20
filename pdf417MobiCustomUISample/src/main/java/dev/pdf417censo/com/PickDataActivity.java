81e70404aa002177749d2d8ec532e6186af75b44
esleyder <megaesleyder@gmail.com>
2023-02-20 12:21:46 -0500

    gitignre

---
+++ pdf417MobiCustomUISample/src/main/java/dev/pdf417censo/com/PickDataActivity.java
@@ -0,0 +1,14 @@
+package dev.pdf417censo.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_data);
+    }
+}
