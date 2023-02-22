c4820373805d161fe5ed10bf92e4e214fe1b60cc
esleyder <megaesleyder@gmail.com>
2023-02-22 16:04:23 -0500

    login activity was created with simple layout

---
+++ pdf417MobiCustomUISample/src/main/java/dev/pdf417censo/com/LoginActivity.java
@@ -0,0 +1,19 @@
+package dev.pdf417censo.com;
+
+import android.os.Bundle;
+import android.widget.NumberPicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private NumberPicker picker1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
---
+++ pdf417MobiCustomUISample/src/main/java/dev/pdf417censo/com/PickDataActivity.java
@@ -0,0 +1,19 @@
+package dev.pdf417censo.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.NumberPicker;
+
+public class PickDataActivity extends AppCompatActivity {
+
+    private NumberPicker picker1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_data);
+
+        picker1 = findViewById(R.id.numberpicker_main_picker);
+    }
+}
