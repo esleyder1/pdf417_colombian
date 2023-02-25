874edc55037b3c48083b2575c833d8005f797cf9
esleyder <megaesleyder@gmail.com>
2023-02-25 17:46:54 -0500

    dropdown menu was created, community and relationship.

--- pdf417MobiCustomUISample/src/main/java/dev/pdf417censo/com/PickDataActivity.java
+++ pdf417MobiCustomUISample/src/main/java/dev/pdf417censo/com/PickDataActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
@@ -15,37 +17,39 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import dev.pdf417censo.com.databinding.ActivityMainBinding;
+import kotlin.LateinitKt;
+
 public class PickDataActivity extends AppCompatActivity {
 
-    private NumberPicker picker1;
+    AutoCompleteTextView textViewCommunity, acRelationship;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_data);
 
+        //Campo: COMUNIDAD INDÍGENA
+        String[] arrayCommunity={"Misak","Nasa"};
+        ArrayAdapter<String> adapterCommunity;
+        textViewCommunity = findViewById(R.id.acCommunity);
+        adapterCommunity = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, arrayCommunity);
+        textViewCommunity.setAdapter(adapterCommunity);
 
-        // Spinner element
-        final Spinner spinner = (Spinner) findViewById(R.id.spCommunity);
+        //Campo: PARENTESCO
+        String[] arrayRelationshipF ={"Madre cabeza de hogar","Abuela","Hija","Prima","Tía", "Nieta", "Sobrina" };
+        String[] arrayRelationshipM ={"Padre cabeza de hogar","Abuelo","Hijo","Primo","Tío", "Nieto", "Sobrino" };
+
+        ArrayAdapter<String> adapter;
+        acRelationship = findViewById(R.id.acRelationship);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, arrayRelationshipM);
+        acRelationship.setAdapter(adapter);
+
+
         Button button = (Button) findViewById(R.id.btnSaveForm);
-
-        // Spinner click listener
-
-
-        // Spinner Drop down elements
-        List<String> categories = new ArrayList<String>();
-        categories.add("INDÍGENA");
-        categories.add("MISAK");
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
