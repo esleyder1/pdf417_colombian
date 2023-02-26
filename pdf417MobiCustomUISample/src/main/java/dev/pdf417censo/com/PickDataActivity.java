4065e2651f00bbe20dbdb9d062811660dd181f3b
esleyder <megaesleyder@gmail.com>
2023-02-25 21:46:58 -0500

    roboto font was updated

--- pdf417MobiCustomUISample/src/main/java/dev/pdf417censo/com/PickDataActivity.java
+++ pdf417MobiCustomUISample/src/main/java/dev/pdf417censo/com/PickDataActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,16 +15,11 @@ import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import dev.pdf417censo.com.databinding.ActivityMainBinding;
-import kotlin.LateinitKt;
-
 public class PickDataActivity extends AppCompatActivity {
 
     AutoCompleteTextView textViewCommunity, acRelationship;
-
+    private NumberPicker picker1;
+    private String[] pickerVals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +43,7 @@ public class PickDataActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, arrayRelationshipM);
         acRelationship.setAdapter(adapter);*/
 
-        //Campo: INTEGRANTES
+        //Campo: VEREDAS - BARRIO
 
         String[] arrayAddresses ={"La Mina","Guayope","El Epiro","Picacho", "La Palma", "Loma Redonda", "La Esperanza", "Barondillo", "Zolapa", "El Tablón" };
         ArrayAdapter<String> adapter;
@@ -57,6 +53,20 @@ public class PickDataActivity extends AppCompatActivity {
         acRelationship.setAdapter(adapter);
 
 
+        //INTEGRANTES DE LA FAMILIA
+        picker1 = findViewById(R.id.numberpicker_main_picker);
+        picker1.setMaxValue(15);
+        picker1.setMinValue(1);
+        pickerVals  = new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17"};
+        picker1.setDisplayedValues(pickerVals);
+
+        picker1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int valuePicker1 = picker1.getValue();
+                Log.d("picker value", pickerVals[valuePicker1]);
+            }
+        });
 
 
         Button button = (Button) findViewById(R.id.btnSaveForm);
